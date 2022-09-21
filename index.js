9e75920634cef8a0f2a0f0395354b62557ef0786
Nahuel Lopez <xxnaxu@gmail.com>
2022-09-21 17:42:43 -0300

    storage

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let productos = [
+/* let productos = [
     { id: 1, nombre: "Paco Rabanne Invictus", precio: 107, imagen: "./images/invictus.jpg", link: "./pages/perfume_1.html" },
     { id: 2, nombre: "Ralph Lauren Ralph's Club", precio: 608, imagen: "./images/ralphsclub.jpg", link: "./pages/perfume_2.html" },
     { id: 3, nombre: "Carolina Herrera 212 Vip", precio: 504, imagen: "./images/212black.jpg", link: "./pages/perfume_3.html" },
@@ -8,7 +8,9 @@ let productos = [
     { id: 7, nombre: "Ralph Lauren Polo Red", precio: 907, imagen: "./images/polored.jpg", link: "./pages/perfume_7.html" },
     { id: 8, nombre: "Polo Blue Edt", precio: 264, imagen: "./images/poloblue.jpg", link: "./pages/perfume_8.html" },
     { id: 9, nombre: "Polo Deep Blue Parfum", precio: 478, imagen: "./images/polodeepblue.jpg", link: "./pages/perfume_9.html" },
-]
+] */
+
+/* localStorage.setItem("productos", JSON.stringify(productos)); */
 
 //BUSCADOR
 /* let perfume = document.getElementById("perfume");
@@ -71,14 +73,24 @@ const carritoIndex = (productoId)=>{
                         <button id="eliminar${producto.id}" class="boton-eliminar" >Eliminar</i></button>`;
 
         contenedorCarrito.appendChild(div)
-
-        const eliminar = document.getElementById( `eliminar${producto.id}` )
-
-        eliminar.removeEventListener('click', ()=> {
-            carritoIndex(producto.id)
-            alert(`Se elimino el PERFUME ${producto.nombre}`)
-        })
     }
 
     renderProductosCarrito()
 }
+
+const eliminar = (productoId) => {
+    const producto = carritoDeCompras.find((producto) => producto.Id === productoId)
+    const indice = carrito.indexOf(producto)
+    carritoDeCompras.splice(indice, 1)
+}
+
+function cambiarPrecio(nombre, precio){
+    let array = JSON.parse(localStorage.getItem('productos'));
+  
+    const producto = array.find(producto => producto.nombre === nombre);
+    producto.precio = precio;
+  
+    localStorage.setItem("productos", JSON.stringify(array));
+}
+
+cambiarPrecio("Paco Rabanne Invictus", 666);
