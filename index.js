e4b4ee52ba87acf1cf996719b246e3c394f10bb2
Nahuel Lopez <xxnaxu@gmail.com>
2022-10-03 18:36:44 -0300

    fetch

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-let productos = [
-    { id: 1, nombre: "Paco Rabanne Invictus", precio: 107, imagen: "./images/invictus.jpg", link: "./pages/perfume_1.html" },
-    { id: 2, nombre: "Ralph Lauren Ralph's Club", precio: 608, imagen: "./images/ralphsclub.jpg", link: "./pages/perfume_2.html" },
-    { id: 3, nombre: "Carolina Herrera 212 Vip", precio: 504, imagen: "./images/212black.jpg", link: "./pages/perfume_3.html" },
-    { id: 4, nombre: "Antonio Banderas The Icon", precio: 812, imagen: "./images/theicon.jpg", link: "./pages/perfume_4.html" },
-    { id: 5, nombre: "Giorgio Armani", precio: 376, imagen: "./images/giorgioarmani.jpg", link: "./pages/perfume_5.html" },
-    { id: 6, nombre: "Azzaro Chrome", precio: 725, imagen: "./images/azzaro.jpg", link: "./pages/perfume_6.html" },
-    { id: 7, nombre: "Ralph Lauren Polo Red", precio: 907, imagen: "./images/polored.jpg", link: "./pages/perfume_7.html" },
-    { id: 8, nombre: "Polo Blue Edt", precio: 264, imagen: "./images/poloblue.jpg", link: "./pages/perfume_8.html" },
-    { id: 9, nombre: "Polo Deep Blue Parfum", precio: 478, imagen: "./images/polodeepblue.jpg", link: "./pages/perfume_9.html" },
-] 
+fetch("./data.json")
+.then(response => response.json())
+.then(data => {
+  data.forEach(producto => {
+    let div = document.createElement("div");
+    div.classList.add("card")
+    div.innerHTML = `
+    <div class="card__header" >
+    <a href="${producto.link}"> <img src="${producto.imagen}" alt="${producto.nombre}" /></a>
+    </div>
+    <a href="${producto.link}"><h3 class="card__title">${producto.nombre}</h3></a>
+    <p class="card__price">${producto.precio}</p>
+    <button id=boton${producto.id}>Comprar</button>
+    `;
+    perfume.append(div);
+
+    const boton = document.getElementById( `boton${producto.id}` )
+
+    boton.addEventListener('click', ()=> {
+      carritoIndex(producto.id)
+      
+      const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 1500,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+        })
+        
+        Toast.fire({
+          icon: 'success',
+          title: `Se agrego el PERFUME ${producto.nombre}`
+        })
+  })
+  });
+})
+
 
 /* localStorage.setItem("productos", JSON.stringify(productos)); */
 
@@ -23,7 +53,7 @@ perfume.innerHTML = `<div class="card__header" >
                      <p class="card__price">${producto.precio}</p>`;  */
 
 //MOSTRAR PRODUCTOS
-const mostrarProductos = (productos) => {
+/* const mostrarProductos = (productos) => {
     const contenedorProductos = document.getElementById("perfume")
 
     productos.forEach(producto => {
@@ -62,7 +92,7 @@ const mostrarProductos = (productos) => {
         })
 
     })
-}
+} */
 
 mostrarProductos(productos)
 
